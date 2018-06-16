24b57ee34db07bda7d5753ff73b33f1098e259d5
박재현 <jhjh@bagjaehyeon-ui-MacBook-Pro.local>
2018-06-16 21:31:31 +0900

    Customizing the Django Admin

--- hellogram/hellogram/images/admin.py
+++ hellogram/hellogram/images/admin.py
@@ -5,19 +5,52 @@ from . import models
 @admin.register(models.Image)
 class ImageAdmin(admin.ModelAdmin):
 
-    pass 
+    search_fields = (
+        'location',
+        'caption',
+    )
+
+    list_filter = (
+        'location',
+        'creator',
+    )
+
+    list_display_links = (
+        'location',
+        'caption',
+    )
+
+    list_display = (
+        'creator',
+        'created_at',
+        'updated_at',
+        'file',
+        'location',
+        'caption'
+    ) 
 
 
 @admin.register(models.Like)
 class LikeAdmin(admin.ModelAdmin):
 
-    pass 
+    list_display = (
+        'creator',
+        'created_at',
+        'updated_at',
+        'image',
+    ) 
 
 
 @admin.register(models.Comment)
 class CommentAdming(admin.ModelAdmin):
 
-    pass 
+    list_display = (
+        'creator',
+        'created_at',
+        'updated_at',
+        'message',
+        'image',
+    ) 
 
 
     
--- hellogram/hellogram/images/models.py
+++ hellogram/hellogram/images/models.py
@@ -19,6 +19,8 @@ class Image(TimeStampedModel):
     caption = models.TextField()
     creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
 
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
 
 class Comment(TimeStampedModel):
 
@@ -35,3 +37,6 @@ class Like(TimeStampedModel):
 
     creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
     image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return 'User : {} / Image Caption : {}'.format(self.creator.username, self.image.caption)
