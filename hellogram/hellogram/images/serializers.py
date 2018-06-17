bb81cf1139f433e2b1190e6be629bf38ff0b003e
박재현 <jhjh@bagjaehyeon-ui-MacBook-Pro.local>
2018-06-17 13:37:39 +0900

    Getting the User Feed

--- hellogram/hellogram/images/serializers.py
+++ hellogram/hellogram/images/serializers.py
@@ -1,13 +1,30 @@
 from rest_framework import serializers
 from . import models 
+from hellogram.users import models as user_models
+
+
+class FeedUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = user_models.User 
+        fields = (
+            'username',
+            'profile_image'
+        )
 
 
 
 class CommentSerializer(serializers.ModelSerializer):
 
+    creator = FeedUserSerializer()
+
     class Meta:
         model = models.Comment
-        fields = '__all__'
+        fields = (
+            'id',
+            'message',
+            'creator',
+        )
 
 
 
@@ -19,18 +36,21 @@ class LikeSerializer(serializers.ModelSerializer):
 
 
 
+
 class ImageSerializer(serializers.ModelSerializer):
 
     comments = CommentSerializer(many=True)
-    likes = LikeSerializer(many=True)
+    creator = FeedUserSerializer()
 
     class Meta:
         model = models.Image 
         fields = (
             'id',
+            'creator',
             'file',
             'location',
             'caption',
             'comments',
-            'likes'
+            'like_count',
+            
         )
