aa7473019665a9c7e63c0254ab3c8c7e9c649f8e
박재현 <jhjh@bagjaehyeon-ui-MacBook-Pro.local>
2018-06-17 20:28:24 +0900

    Get people to follow on :explore

--- hellogram/hellogram/users/models.py
+++ hellogram/hellogram/users/models.py
@@ -21,8 +21,8 @@ class User(AbstractUser):
     bio = models.TextField(null=True)
     phone = models.CharField(max_length=140, null=True)
     gender = models.CharField(max_length=80, choices=GENDER_CHOICES, null=True)
-    follower = models.ManyToManyField("self")
-    following = models.ManyToManyField("self")
+    follower = models.ManyToManyField("self", blank=True)
+    following = models.ManyToManyField("self", blank=True)
 
 
     def __str__(self):
