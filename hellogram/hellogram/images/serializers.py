65997d8c3c6d721da77fe386479aa16ae4b7e18c
박재현 <jhjh@bagjaehyeon-ui-MacBook-Pro.local>
2018-07-09 02:29:49 +0900

    serializing the tags

--- hellogram/hellogram/images/serializers.py
+++ hellogram/hellogram/images/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from taggit_serializer.serializers import (TagListSerializerField,
+                                           TaggitSerializer)
 from . import models 
 from hellogram.users import models as user_models
 
@@ -60,10 +62,11 @@ class LikeSerializer(serializers.ModelSerializer):
 
 
 
-class ImageSerializer(serializers.ModelSerializer):
+class ImageSerializer(TaggitSerializer, serializers.ModelSerializer):
 
     comments = CommentSerializer(many=True)
     creator = FeedUserSerializer()
+    tags = TagListSerializerField()
 
     class Meta:
         model = models.Image 
@@ -75,6 +78,7 @@ class ImageSerializer(serializers.ModelSerializer):
             'caption',
             'comments',
             'like_count',
+            'tags',
             'created_at',
         )
 
