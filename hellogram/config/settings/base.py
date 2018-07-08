a1c1454ae55d89c815957c0631456be89c262be9
박재현 <jhjh@bagjaehyeon-ui-MacBook-Pro.local>
2018-07-09 01:28:42 +0900

    setting up JWT

--- hellogram/config/settings/base.py
+++ hellogram/config/settings/base.py
@@ -241,3 +241,15 @@ SOCIALACCOUNT_ADAPTER = 'hellogram.users.adapters.SocialAccountAdapter'
 # Your stuff...
 # ------------------------------------------------------------------------------
 TAGGIT_CASE_INSENSITIVE = True 
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
